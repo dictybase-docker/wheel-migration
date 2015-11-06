00f8b411462fc9bcdc25c7545b60833354d9fc6a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-11-06 07:20:03 -0600

    Added a function to register with etcd

--- create_chado_user.sh
+++ create_chado_user.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-if [ -e "/secrets/chadouser" ]
-then
-    CHADO_USER=cat $(cat /etc/chadouser)
-fi
+register_etcd() {
+    if [ ${ETCD_CLIENT_SERVICE_HOST+defined} ]
+    then
+        curl http://${ETCD_CLIENT_SERVICE_HOST}:${ETCD_CLIENT_SERVICE_PORT}/v2/keys/migration/postgresql -XPUT -d value="complete"
+    else
+        echo "did not register with etcd"
+    fi
+}
 
-if [ -e "/secrets/chadodb" ]
-then
-    CHADO_DB=cat $(cat /etc/chadodb)
-fi
+extract_secret(){
+    [ -e "/secrets/chadouser" ] && CHADO_USER=cat $(cat /etc/chadouser)
+    [ -e "/secrets/chadodb" ] && CHADO_DB=cat $(cat /etc/chadodb)
+    [ -e "/secrets/chadouser" ] && CHADO_USER=cat $(cat /etc/chadopass)
+}
 
-if [ -e "/secrets/chadouser" ]
-then
-    CHADO_USER=cat $(cat /etc/chadopass)
-fi
-
-
-if [ "${CHADO_USER+defined}" -a "${CHADO_PASS+defined}" -a "${CHADO_DB+defined}" ]
-then
-        psql --username postgres <<-EOSQL
-            CREATE ROLE $CHADO_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$CHADO_PASS';
-            CREATE DATABASE $CHADO_DB OWNER $CHADO_USER;
+create_chado_user() {
+    if [ "${CHADO_USER+defined}" -a "${CHADO_PASS+defined}" -a "${CHADO_DB+defined}" ]
+    then
+            psql --username postgres <<-EOSQL
+                CREATE ROLE $CHADO_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$CHADO_PASS';
+                CREATE DATABASE $CHADO_DB OWNER $CHADO_USER;
 EOSQL
-fi
+    fi
+}
+
+main() {
+    extract_secret
+    create_chado_user
+    register_etcd
+}
+
+main
+
+
+
