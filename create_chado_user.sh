60c992a584a480ae37c7b0ec3d1fa0514712fc20
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-11-07 06:23:45 -0600

    Removed inline if else Fixed the path for secrets

--- create_chado_user.sh
+++ create_chado_user.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 register_etcd() {
     if [ ${ETCD_CLIENT_SERVICE_HOST+defined} ]
@@ -9,18 +10,29 @@ register_etcd() {
     fi
 }
 
-extract_secret(){
-    [ -e "/secrets/chadouser" ] && CHADO_USER=cat $(cat /etc/chadouser)
-    [ -e "/secrets/chadodb" ] && CHADO_DB=cat $(cat /etc/chadodb)
-    [ -e "/secrets/chadouser" ] && CHADO_USER=cat $(cat /etc/chadopass)
+extract_secret() {
+   if [ -e "/secrets/chadouser" ] 
+   then
+       CHADO_USER=$(cat /secrets/chadouser)
+   fi
+
+   if [ -e "/secrets/chadodb" ] 
+   then
+       CHADO_DB=$(cat /secrets/chadodb)
+   fi
+
+   if [ -e "/secrets/chadouser" ] 
+   then
+       CHADO_USER=$(cat /secrets/chadopass)
+   fi
 }
 
 create_chado_user() {
     if [ "${CHADO_USER+defined}" -a "${CHADO_PASS+defined}" -a "${CHADO_DB+defined}" ]
     then
-            psql --username postgres <<-EOSQL
-                CREATE ROLE $CHADO_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$CHADO_PASS';
-                CREATE DATABASE $CHADO_DB OWNER $CHADO_USER;
+        psql --username postgres <<-EOSQL
+            CREATE ROLE $CHADO_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$CHADO_PASS';
+            CREATE DATABASE $CHADO_DB OWNER $CHADO_USER;
 EOSQL
     fi
 }
